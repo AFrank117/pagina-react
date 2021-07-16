8ed213b4848c32b28b4ed5ff668329bd74e6d04b
AFrank117 <frankluisgomezgutierrez@gmail.com>
2021-07-16 11:17:49 -0500

    correccion

--- src/Components/C01componente.js
+++ src/Components/C01componente.js
@@ -3,7 +3,7 @@ import react from 'react'
 const C01componente = () => {
     return (
         <div>
-
+            componente 1. definido con funcion flecha(rafce)
         </div>
     )
 }
--- src/Components/C02contador.js
+++ src/Components/C02contador.js
@@ -3,7 +3,7 @@ import react from 'react'
 const C02contador = () => {
     return (
         <div>
-
+            aqui codigo contador
         </div>
     )
 }
